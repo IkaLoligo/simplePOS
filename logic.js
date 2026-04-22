59addd531b846ce5e384431f4f4d3fcfd5fa56f6
IkaLoligo <merlijnveen01@gmail.com>
2026-04-22 09:38:11 +0200

    Initial push
    Simple HTML and CSS done, optimized for iphone screen. JS added the button and text vars

---
+++ logic.js
@@ -0,0 +1,25 @@
+// Variable text 
+var selected = document.getElementById("selected")
+var count = document.getElementById("count")
+var todayTotalNumber = document.getElementById("todayTotalNumber")
+
+//buttons Products
+var product1 = document.getElementById("itemButton1")
+var product2 = document.getElementById("itemButton2")
+var product3 = document.getElementById("itemButton3")
+var product4 = document.getElementById("itemButton4")
+var product5 = document.getElementById("itemButton5")
+var product6 = document.getElementById("itemButton6")
+var product7 = document.getElementById("itemButton7")
+var product8 = document.getElementById("itemButton8")
+var product9 = document.getElementById("itemButton9")
+var product10 = document.getElementById("itemButton10")
+
+//buttons manipulators
+var addOne = document.getElementById("addOne")
+var administerProduct = document.getElementById("administerProduct")
+var selectLastProduct = document.getElementById("selectLastProduct")
+var selectedProductDelete = document.getElementById("selectedProductDelete")
+var todayTotalList = document.getElementById("todayTotalList")
+
+
