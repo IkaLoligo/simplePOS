e7c8da00dc4268215534352cb8e1e984325ecffd
IkaLoligo <merlijnveen01@gmail.com>
2026-06-11 13:44:03 +0200

    kladje

---
+++ functionKlad.js
@@ -0,0 +1,7 @@
+function buttonsInit () {
+    //delete all childeren of productButtonBox
+    //let findMainCategories = buttonsStored.filter((category) => category.category === 'mainCategory') (works)
+    //for loop that loops through findMainCategories that does:
+    //      add button child to productButtonBox with id that == findMainCategories[arrayCount].buttonID
+    
+}
